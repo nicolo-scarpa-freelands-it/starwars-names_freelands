b2d275e73778460a9a5a1647cb05d6f4b8be033f
Nicolò Scarpa <nicolo.scarpa@freelands.it>
2016-12-21 15:24:02 +0100

    Adds real tests for  and  methods

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,30 @@
 var expect = require('chai').expect;
 var starWars = require('./index');
 
+// mocha global "descibe" function
 describe('starwars-name', function() {
-  it('should work!', function() {
-    expect(true).to.be.true;
+  describe('all', function() {
+    // mocha global "it" function
+    it('should be an array of strings', function() {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string';
+        });
+      }
+    });
+
+    it('should contain `Luke Skywalker`', function() {
+      expect(starWars.all).to.include('Luke Skywalker');
+    });
+  });
+
+  describe('random', function() {
+    it('should return a random item from starWars.all', function() {
+      var randomItem = starWars.random();
+
+      expect(starWars.all).to.include(randomItem);
+    });
   });
 });
